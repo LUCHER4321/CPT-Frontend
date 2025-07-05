84619f549f3b26413b0ebc2ecf103d3f9e0ea310
LUCHER4321 <lucher4321@gmail.com>
2025-07-04 20:10:15 -0400

    Create enums.tsx

---
+++ src/enums.tsx
@@ -0,0 +1,37 @@
+export enum Liked {
+    TREE = "ph-tree",
+    COMMENT = "comment"
+}
+
+export enum NotiFunc {
+    FOLLOW = "follow",
+    TREE = "tree",
+    COMMENT = "comment",
+    LIKE = "like"
+}
+
+export enum Role {
+    ADMIN = "admin",
+    USER = "user",
+    BOSS = "boss"
+}
+
+export enum Plan {
+    FREE = "free",
+    PRO = "pro",
+    PREMIUM = "premium"
+}
+
+export enum TreeCriteria {
+    CREATED_AT = "createdAt",
+    UPDATED_AT = "updatedAt",
+    LIKES = "likes",
+    COMMENTS = "comments",
+    VIEWS = "views",
+    NAME = "name"
+}
+
+export enum Order {
+    ASC = "asc",
+    DESC = "desc"
+}
