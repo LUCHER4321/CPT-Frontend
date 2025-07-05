c2894ba0d589c2b5f6c0d91295fc0cf1a37c7366
LUCHER4321 <lucher4321@gmail.com>
2025-07-05 11:48:39 -0400

    Create species.tsx

---
+++ src/api/species.tsx
@@ -0,0 +1,35 @@
+import type { SpeciesRequest } from "../types";
+import { fetchConfig, fetchImage } from "../utils/forAPI";
+
+const species = (treeId: string, ...r: string[]) => ["species", treeId, ...r];
+
+export const speciesRequest: SpeciesRequest = {
+    createSpecies: async ({ treeId, ...body }) => await fetchConfig({
+        method: "POST",
+        route: species(treeId),
+        body
+    }),
+    updateSpecies: async ({ treeId, id, ...body }) => await fetchConfig({
+        method: "PATCH",
+        route: species(treeId, id),
+        body
+    }),
+    deleteSpecies: async ({ treeId, id }) => await fetchConfig({
+        method: "DELETE",
+        route: species(treeId, id)
+    }),
+    speciesImage: async ({ treeId, id, image }) => await fetchImage({
+        route: species(treeId, id, "image"),
+        image
+    }),
+    deleteSpeciesImage: async ({ treeId, id }) => await fetchConfig({
+        method: "DELETE",
+        route: species(treeId, id, "image")
+    }),
+    treeSpecies: async ({ treeId }) => await fetchConfig({
+        route: species(treeId)
+    }),
+    getSpecies: async ({ treeId, id }) => await fetchConfig({
+        route: species(treeId, id)
+    })
+}
