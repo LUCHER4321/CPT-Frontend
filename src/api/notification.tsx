8be0cf7423a36efefaebf69c13501646e31b9ad0
LUCHER4321 <lucher4321@gmail.com>
2025-07-05 11:55:16 -0400

    Create notification.tsx

---
+++ src/api/notification.tsx
@@ -0,0 +1,15 @@
+import type { NotificationRequest } from "../types";
+import { fetchConfig } from "../utils/forAPI";
+
+const notification = "notification";
+
+export const notificationRequest: NotificationRequest = {
+    getNotifications: async (queries) => await fetchConfig({
+        route: [notification],
+        queries
+    }),
+    seeNotification: async ({ id }) => await fetchConfig({
+        method: "PATCH",
+        route: [notification, id]
+    })
+}
