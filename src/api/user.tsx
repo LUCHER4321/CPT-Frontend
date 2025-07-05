1a1d5f1b4bd26fe86f7d98976f6f789c9325e58f
LUCHER4321 <lucher4321@gmail.com>
2025-07-04 23:04:51 -0400

    Create user.tsx

---
+++ src/api/user.tsx
@@ -0,0 +1,68 @@
+import type { UserRequest } from "../types";
+import { fetchConfig, fetchImage } from "../utils/forAPI";
+
+const user = "user";
+const userMe = ["user", "me"];
+
+export const userRequest: UserRequest = {
+    register: async (body) => await fetchConfig({
+        method: "POST",
+        body,
+        route: [user, "register"]
+    }),
+    login: async (body) => await fetchConfig({
+        method: "POST",
+        body,
+        route: [user, "login"]
+    }),
+    search: async (queries) => await fetchConfig({
+        route: [user, "search"],
+        queries
+    }),
+    getUser: async ({ id }) => await fetchConfig({
+        route: [user, id]
+    }),
+    logout: async (body) => await fetchConfig({
+        method: "POST",
+        body,
+        route: [user, "logout"]
+    }),
+    admin: async (body) => await fetchConfig({
+        method: "POST",
+        body,
+        route: [user, "admin"]
+    }),
+    token: async (body) => await fetchConfig({
+        method: "POST",
+        body,
+        route: [user, "token"]
+    }),
+    getMe: async () => await fetchConfig({
+        route: userMe
+    }),
+    updateMe: async (body) => await fetchConfig({
+        method: "PATCH",
+        body,
+        route: userMe
+    }),
+    deleteMe: async () => await fetchConfig({
+        method: "DELETE",
+        route: userMe
+    }),
+    photoMe: async ({ image }) => await fetchImage({
+        route: [...userMe, "photo"],
+        image
+    }),
+    deletePhotoMe: async () => await fetchConfig({
+        method: "DELETE",
+        route: [...userMe, "photo"]
+    }),
+    newApiKey: async () => await fetchConfig({
+        method: "POST",
+        route: [...userMe, "key"]
+    }),
+    deleteApiKey: async ({ keyToDelete }) => await fetchConfig({
+        method: "DELETE",
+        route: [...userMe, "key", keyToDelete]
+    })
+};
