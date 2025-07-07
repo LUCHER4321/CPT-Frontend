841634a24ff48da60cdf58cf3cc2c67864d1f9c8
LUCHER4321 <lucher4321@gmail.com>
2025-07-06 23:40:51 -0400

    Create TopTrees.tsx

---
+++ src/components/TopTrees.tsx
@@ -0,0 +1,22 @@
+import { title } from "../data/classNames";
+import type { PhTreeResponse } from "../types";
+import { TreeCard } from "./TreeCard";
+
+interface TopTreesProps {
+    id?: string;
+    trees?: (PhTreeResponse & { username: string; })[]
+}
+
+export const TopTrees = ({
+    id,
+    trees = []
+}: TopTreesProps) => {
+    return (
+        <section id={id} className="text-center p-16">
+            <h2 className={"text-[2rem] mb-12 " + title}>Featured Trees</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
+                {trees.map(t => (<TreeCard tree={t}/>))}
+            </div>
+        </section>
+    )
+};
