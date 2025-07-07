2a8430e2d893180f9f79357ed247dcbaafdeda41
LUCHER4321 <lucher4321@gmail.com>
2025-07-07 16:52:32 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Auth } from './routes/auth.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App/>}/>
+        <Route path="auth" element={<Auth initialRegister={new URLSearchParams(window.location.search).get("register") === "true"}/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
