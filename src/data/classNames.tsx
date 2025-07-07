180cd475075553a781e5c8e2dea632f2a47ddda6
LUCHER4321 <lucher4321@gmail.com>
2025-07-06 23:39:50 -0400

    Create classNames.tsx

---
+++ src/data/classNames.tsx
@@ -0,0 +1,5 @@
+export const filledButton = "dark:text-black text-white rounded-[2rem]! bg-green-800! dark:bg-lime-300! hover:bg-black! dark:hover:bg-white!";
+export const borderButton = "text-black dark:text-white rounded-[2rem]! border-green-800! dark:border-lime-300! hover:bg-green-800/100! dark:hover:bg-lime-300/100! bg-black/0! hover:text-white dark:hover:text-black";
+export const blankButton = "text-black dark:text-white bg-black/0! rounded-[2rem]! border-black/0! px-2!"
+export const title = "font-bold text-green-800 dark:text-lime-300"
+export const card = "rounded text-center duration-300 hover:-translate-y-[10px] shadow-lg bg-neutral-100 dark:bg-neutral-800";
