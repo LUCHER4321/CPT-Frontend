80a3bde236c3f27309bf43adc8c2905899c8f570
LUCHER4321 <lucher4321@gmail.com>
2025-07-07 16:52:30 -0400

    Create Auth.tsx

---
+++ src/routes/Auth.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react"
+import { LoginForm, RegisterForm } from "../components/AuthForm";
+import { AuthBanner } from "../components/AuthBanner";
+
+interface AuthProps {
+    initialRegister?: boolean;
+}
+
+export const Auth = ({
+    initialRegister = false
+}: AuthProps) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [visiblePassword, setVisiblePassword] = useState(false);
+    const [register, setRegister] = useState(initialRegister);
+    const [visiblePasswordR, setVisiblePasswordR] = useState(false);
+    const [username, setUsername] = useState("");
+    const [emailR, setEmailR] = useState("");
+    const [passwordR, setPasswordR] = useState("");
+    const [passwordC, setPasswordC] = useState("");
+    const [accept, setAccept] = useState(false);
+    useEffect(() => {
+        document.title = `Life Tree | ${register ? "Register" : "Log In"}`
+    }, [register]);
+    return (
+        <div className="flex flex-col sm:flex-row w-screen sm:min-h-[100vh] p-0!">
+            <div className="sm:min-w-125 px-8 py-12">
+                <LoginForm
+                    email={email}
+                    setEmail={setEmail}
+                    password={password}
+                    setPassword={setPassword}
+                    visiblePassword={visiblePassword}
+                    setVisiblePassword={setVisiblePassword}
+                    className={register ? "hidden" : ""}
+                    setRegister={setRegister}
+                />
+                <RegisterForm
+                    username={username}
+                    setUsername={setUsername}
+                    email={emailR}
+                    setEmail={setEmailR}
+                    password={passwordR}
+                    setPassword={setPasswordR}
+                    confirmPassword={passwordC}
+                    setConfirmPassword={setPasswordC}
+                    visiblePassword={visiblePasswordR}
+                    setVisiblePassword={setVisiblePasswordR}
+                    accept={accept}
+                    setAccept={setAccept}
+                    className={register ? "" : "hidden"}
+                />
+            </div>
+            <AuthBanner/>
+        </div>
+    )
+}
