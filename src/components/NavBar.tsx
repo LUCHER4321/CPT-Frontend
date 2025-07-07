1494bf1510a09735b01d66ada19619362e76d4f2
LUCHER4321 <lucher4321@gmail.com>
2025-07-07 16:51:05 -0400

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ export const NavBar = ({
     children
 }: NavBarProps) => {
     return (
-        <nav className="z-10 flex justify-between bg-lime-300 dark:bg-green-800 fixed left-0 right-0 top-0 py-4 px-8 sm:px-20">
+        <nav className="z-10 flex justify-between bg-[#D8EDD9] dark:bg-[#1B5E20] fixed left-0 right-0 top-0 py-4 px-8 sm:px-20">
             { children }
         </nav>
     );
