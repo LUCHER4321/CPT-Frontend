a22360fee990101219836d323fe983c6fcc7473c
LUCHER4321 <lucher4321@gmail.com>
2025-07-07 16:50:41 -0400

    Update BurgerMenu.tsx

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -9,14 +9,14 @@ export const BurgerMenu = ({
     setOpen,
     open
 }: BurgerMenuProps) => {
-    const onClick = setOpen ? () => setOpen(!(open ?? false)) : () => {};
+    const onClick = () => setOpen?.(!(open ?? false));
     return (
         <>
             <div className="hidden sm:flex flex-row">
                 {children}
             </div>
             <button className={open ? "sm:hidden fixed top-0 bottom-0 left-0 right-0 bg-black/10! rounded-0!" : "hidden"} onClick={onClick}/>
-            <div className={open ? "sm:hidden fixed top-0 bottom-1/2 left-1/2 right-0 flex flex-col justify-center bg-lime-300 dark:bg-green-800 p-5 -z-1" : "hidden"}>
+            <div className={open ? "sm:hidden fixed top-20 left-1/2 right-0 flex flex-col justify-center text-end bg-[#D8EDD9] dark:bg-[#1B5E20] p-5 -z-1" : "hidden"}>
                 {children}
             </div>
             <button className="none flex sm:hidden h-full items-center bg-black/0! p-0!" onClick={onClick}>
