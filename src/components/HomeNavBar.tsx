ae8c038c9442e85061dd6439f506e62984bcf00f
LUCHER4321 <lucher4321@gmail.com>
2025-07-07 18:28:13 -0400

    Update HomeNavBar.tsx

--- src/components/HomeNavBar.tsx
+++ src/components/HomeNavBar.tsx
@@ -1,17 +1,22 @@
 import { blankButton, borderButton, filledButton } from "../data/classNames";
 import { BurgerMenu } from "./BurgerMenu";
 import { HomeButton } from "./HomeButton";
+import { IconInput } from "./IconInput";
 import { LinkButton } from "./LinkButton";
 import { NavBar } from "./NavBar";
 
 interface HomeNavBarProps {
     open?: boolean;
     setOpen?: (o: boolean) => void;
+    search?: string;
+    setSearch?: (s: string) => void;
 }
 
 export const HomeNavBar = ({
     open,
-    setOpen
+    setOpen,
+    search,
+    setSearch
 }: HomeNavBarProps) => {
     return (
         <>
@@ -22,7 +27,15 @@ export const HomeNavBar = ({
                         open={open}
                         setOpen={setOpen}
                     >
-                        <input type="text" placeholder="search trees..." className="bg-[#1B5E20] dark:bg-[#D8EDD9] placeholder-[#D8EDD9] dark:placeholder-[#1B5E20] px-4 mx-1 rounded-[2rem]"/>
+                        <IconInput
+                            type="text"
+                            placeholder="search trees..."
+                            className="bg-[#1B5E20] dark:bg-[#D8EDD9] placeholder-white text-white dark:placeholder-black dark:text-black px-4 mx-1 rounded-[2rem] h-full"
+                            value={search}
+                            setValue={setSearch}
+                            icon="fa-search text-white dark:text-black"
+                            buttonClassName={(search?.length ?? 0) > 0 ? "" : "cursor-not-allowed!"}
+                        />
                         <LinkButton href="/#top-trees" className={"m-1 " + blankButton}>Featured Trees</LinkButton>
                         <LinkButton className={"m-1 " + blankButton}>Pricing</LinkButton>
                         <LinkButton href="/auth" className={"m-1 " + borderButton}>Log In</LinkButton>
