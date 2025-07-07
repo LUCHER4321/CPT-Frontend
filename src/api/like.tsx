a184cdb6717cb7c73e64106b461a899aa30ada46
LUCHER4321 <lucher4321@gmail.com>
2025-07-06 23:39:23 -0400

    Update like.tsx

--- src/api/like.tsx
+++ src/api/like.tsx
@@ -2,24 +2,32 @@ import { Liked } from "../enums";
 import type { LikeRequest } from "../types";
 import { fetchConfig } from "../utils/forAPI";
 
-const like = (liked: Liked, ...r: string[]) => ["like", liked, ...r];
+const likeUrl = (liked: Liked, ...r: string[]) => ["like", liked, ...r];
 
-export const likeRequest: LikeRequest = {
+const likeRequest: LikeRequest = {
     like: async ({ liked, id }) => await fetchConfig({
         method: "POST",
-        route: like(liked, id)
+        route: likeUrl(liked, id)
     }),
     unlike: async ({ liked, id }) => await fetchConfig({
         method: "DELETE",
-        route: like(liked, id)
+        route: likeUrl(liked, id)
     }),
     likedTrees: async () => await fetchConfig({
-        route: like(Liked.TREE, "liked")
+        route: likeUrl(Liked.TREE, "liked")
     }),
     likedComments: async () => await fetchConfig({
-        route: like(Liked.COMMENT, "liked")
+        route: likeUrl(Liked.COMMENT, "liked")
     }),
     getLikes: async ({ liked, id }) => await fetchConfig({
-        route: like(liked, id)
+        route: likeUrl(liked, id)
     })
 }
+
+export const {
+    like,
+    unlike,
+    likedTrees,
+    likedComments,
+    getLikes
+} = likeRequest;
