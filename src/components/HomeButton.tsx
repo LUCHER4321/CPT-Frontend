a18968639109ffb0b4abcfabb37ec7d7d1a55b69
LUCHER4321 <lucher4321@gmail.com>
2025-07-07 16:50:56 -0400

    Update HomeButton.tsx

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -6,6 +6,6 @@ export const HomeButton = ({
     href = "/"
 }: HomeButtonProps) => {
     return (
-        <a href={href}><div className="text-[2rem] font-black text-green-800 dark:text-lime-300">Life Tree</div></a>
+        <a href={href}><div className="text-[2rem] font-black text-[#1B5E20]! dark:text-[#D8EDD9]!">Life Tree</div></a>
     );
 };
