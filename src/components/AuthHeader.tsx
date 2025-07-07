a8c28d5d914ee7eb6d983e350963acef139113cd
LUCHER4321 <lucher4321@gmail.com>
2025-07-07 16:52:19 -0400

    Create AuthHeader.tsx

---
+++ src/components/AuthHeader.tsx
@@ -0,0 +1,20 @@
+import { title } from "../data/classNames";
+
+interface AuthHeaderProps {
+    title?: string;
+    subTitle?: string;
+    className?: string;
+}
+
+export const AuthHeader = ({
+    title: headerTitle,
+    subTitle,
+    className
+}: AuthHeaderProps) => {
+    return (
+        <div className={"w-full text-center mb-8 " + className}>
+            <h2 className={"text-[2rem] mb-2 " + title}>{headerTitle}</h2>
+            <p>{subTitle}</p>
+        </div>
+    )
+}
