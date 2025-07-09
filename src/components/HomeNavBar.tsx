ac459fafaae7a3c5ff6210ed4f3559d14e7d5d5e
LUCHER4321 <lucher4321@gmail.com>
2025-07-09 03:07:11 -0400

    Update HomeNavBar.tsx

--- src/components/HomeNavBar.tsx
+++ src/components/HomeNavBar.tsx
@@ -30,14 +30,14 @@ export const HomeNavBar = ({
                         <IconInput
                             type="text"
                             placeholder="search trees..."
-                            className="bg-[#1B5E20] dark:bg-[#D8EDD9] placeholder-white text-white dark:placeholder-black dark:text-black px-4 mx-1 rounded-[2rem] h-full"
+                            className="bg-[#1B5E20] dark:bg-[#D8EDD9] placeholder-white text-white dark:placeholder-black dark:text-black px-4 mx-1 rounded-full h-full"
                             value={search}
                             setValue={setSearch}
                             icon="fa-search text-white dark:text-black"
                             buttonClassName={(search?.length ?? 0) > 0 ? "" : "cursor-not-allowed!"}
                         />
                         <LinkButton href="/#top-trees" className={"m-1 " + blankButton}>Featured Trees</LinkButton>
-                        <LinkButton className={"m-1 " + blankButton}>Pricing</LinkButton>
+                        <LinkButton href="/pricing" className={"m-1 " + blankButton}>Pricing</LinkButton>
                         <LinkButton href="/auth" className={"m-1 " + borderButton}>Log In</LinkButton>
                         <LinkButton href="/auth?register=true" className={"m-1 " + filledButton}>Register</LinkButton>
                     </BurgerMenu>
