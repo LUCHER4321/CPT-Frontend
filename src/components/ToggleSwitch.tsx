9428c6b8d69d7672d0e730a726001cd2f836e9be
LUCHER4321 <lucher4321@gmail.com>
2025-07-09 03:09:06 -0400

    Create ToggleSwitch.tsx

---
+++ src/components/ToggleSwitch.tsx
@@ -0,0 +1,30 @@
+interface ToggleSwitchProps {
+    id?: string;
+    peer?: string;
+    checked?: boolean;
+    onChange?: (b: boolean) => void;
+    className?: string;
+    spanClassName?: string;
+}
+
+export const ToggleSwitch = ({
+    id,
+    peer = "peer",
+    checked,
+    onChange,
+    className,
+    spanClassName
+}: ToggleSwitchProps) => {
+    return (
+        <label htmlFor={id} className={"aspect-2/1 rounded-full relative cursor-pointer transition-all duration-500 " + className}>
+            <input
+                type="checkbox"
+                id={id}
+                checked={checked}
+                onChange={e => onChange?.(e.target.checked)}
+                className={"sr-only " + peer}
+            />
+            <span className={"w-2/5 aspect-square absolute rounded-full left-1/20 top-1/10 peer-checked:left-11/20 transition-all duration-500 " + spanClassName}/>
+        </label>
+    )
+}
