3c34686db7ec63bfbcc35d8558223200f150061b
LUCHER4321 <lucher4321@gmail.com>
2025-07-09 03:07:39 -0400

    Update classNames.tsx

--- src/data/classNames.tsx
+++ src/data/classNames.tsx
@@ -1,6 +1,8 @@
-export const filledButton = "dark:text-black text-white rounded-[2rem]! bg-[#1B5E20]! dark:bg-[#D8EDD9]! hover:bg-black! dark:hover:bg-white!";
-export const borderButton = "text-black dark:text-white rounded-[2rem]! border-[#1B5E20]! dark:border-[#D8EDD9]! hover:bg-[#1B5E20]/100! dark:hover:bg-[#D8EDD9]/100! bg-black/0! hover:text-white dark:hover:text-black";
-export const blankButton = "text-black dark:text-white bg-black/0! rounded-[2rem]! border-black/0! px-2!"
+export const filledButton = "dark:text-black text-white rounded-full! bg-[#1B5E20]! dark:bg-[#D8EDD9]! hover:bg-black! dark:hover:bg-white!";
+export const borderButton = "text-black dark:text-white rounded-full! border-[#1B5E20]! dark:border-[#D8EDD9]! hover:bg-[#1B5E20]/100! dark:hover:bg-[#D8EDD9]/100! bg-black/0! hover:text-white dark:hover:text-black";
+export const blankButton = "text-black dark:text-white bg-black/0! rounded-full! border-black/0! px-2!"
 export const title = "font-bold text-[#1B5E20]! dark:text-[#D8EDD9]!"
 export const card = "rounded text-center duration-300 hover:-translate-y-[10px] shadow-lg bg-neutral-100 dark:bg-neutral-800";
 export const aText = "text-[#1B5E20]! dark:text-[#D8EDD9]! hover:underline! cursor-pointer"
+export const selectedBilling = "text-[#1B5E20] dark:text-[#D8EDD9] font-semibold text-lg transition-all duration-500"
+export const unselectedBilling = "font-medium text-lg transition-all duration-500"
