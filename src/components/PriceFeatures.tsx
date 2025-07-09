0a3e42d20934f4251bc59422a2be9ba56dfcd2d7
LUCHER4321 <lucher4321@gmail.com>
2025-07-09 03:10:27 -0400

    Create PriceFeatures.tsx

---
+++ src/components/PriceFeatures.tsx
@@ -0,0 +1,28 @@
+import { plans } from "../data/prices";
+import type { Plan } from "../enums";
+import { PlanFeatures } from "./PlanFeatures";
+
+interface PriceFeaturesProps {
+    plan?: Plan;
+}
+
+export const PriceFeatures = ({
+    plan
+}: PriceFeaturesProps) => {
+    if(!plan) return <></>;
+    const {
+        maxTrees,
+        maxSpecies,
+        maxCollaborators
+    } = plans.get(plan)?.constraints ?? {};
+    return (
+        <PlanFeatures>
+            {`${maxTrees ? `Up to ${maxTrees}` : "Unlimited"} phylogenetic trees
+            ${maxSpecies ? `Up to ${maxSpecies}` : "Unlimited"} species per tree
+            ${maxCollaborators ? `Up to ${maxCollaborators} collaborators per tree` : ""}
+            Upload images many formats (jpg, jpeg, png, gif, svg)
+            Professional visualization
+            Access to phylogenetic trees created by the community`}
+        </PlanFeatures>
+    )
+}
