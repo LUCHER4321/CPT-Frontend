a36483692cc27fbabc57876f8c8ca3a27778a10d
LUCHER4321 <lucher4321@gmail.com>
2025-07-09 03:08:13 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Auth } from './routes/auth.tsx'
+import { Auth } from './routes/Auth.tsx'
+import { Pricing } from './routes/Pricing.tsx'
+import { Billing, Plan } from './enums.tsx'
+import { isEnum } from './utils/isEnum.tsx'
+
+const plan = new URLSearchParams(window.location.search).get("plan") ?? "";
+const billing = new URLSearchParams(window.location.search).get("billing") ?? "";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route index element={<App/>}/>
-        <Route path="auth" element={<Auth initialRegister={new URLSearchParams(window.location.search).get("register") === "true"}/>}/>
+        <Route path="auth" element={<Auth
+          initialRegister={new URLSearchParams(window.location.search).get("register") === "true"}
+          initialPlan={isEnum(Plan, plan) ? plan as Plan : undefined}
+          initialBilling={isEnum(Billing, billing) ? billing as Billing : undefined}
+        />}/>
+        <Route path="pricing" element={<Pricing/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
