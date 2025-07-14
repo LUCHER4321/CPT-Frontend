cb4ef1a4f0575e03843d586b6856fabc5b98824f
LUCHER4321 <lucher4321@gmail.com>
2025-07-13 22:30:28 -0400

    Update BurgerMenu.tsx

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -16,11 +16,11 @@ export const BurgerMenu = ({
                 {children}
             </div>
             <button className={open ? "sm:hidden fixed top-0 bottom-0 left-0 right-0 bg-black/10! rounded-0!" : "hidden"} onClick={onClick}/>
-            <div className={open ? "sm:hidden fixed top-20 left-1/2 right-0 flex flex-col justify-center text-end bg-[#D8EDD9] dark:bg-[#1B5E20] p-5 -z-1" : "hidden"}>
+            <div className={"sm:hidden fixed top-20 left-1/2 right-0 flex flex-col justify-center text-center bg-[#D8EDD9] dark:bg-[#1B5E20] -z-1 transition-[max-height] duration-300 " + (open ? "p-5! max-h-[50vh]" : "max-h-0! overflow-hidden")}>
                 {children}
             </div>
-            <button className="none flex sm:hidden h-full items-center bg-black/0! p-0!" onClick={onClick}>
-                <i className="fas fa-bars"/>
+            <button className="none flex sm:hidden h-full items-center bg-black/0! p-0! border-black/0! focus:outline-0!" onClick={onClick}>
+                <i className={"fas transition-all duration-300 " + (open ? "fa-times" : "fa-bars")}/>
             </button>
         </>
     )
