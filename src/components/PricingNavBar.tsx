ae1df424f4c7602153fbabe7dd103607adaa29b8
LUCHER4321 <lucher4321@gmail.com>
2025-07-13 20:26:04 -0400

    Update PricingNavBar.tsx

--- src/components/PricingNavBar.tsx
+++ src/components/PricingNavBar.tsx
@@ -5,8 +5,10 @@ export const PricingNavBar = () => {
     return(
         <>
             <NavBar className="p-[1.5rem]!">
-                <div></div>
-                <h1 className={"text-[1.5rem]! font-semibold " + title}>Plans & Prices</h1>
+                <a className="w-10 aspect-square justify-center flex items-center" href="/">
+                    <i className={"fas fa-arrow-left " + title}/>
+                </a>
+                <h1 className={"text-[1.5rem]! font-semibold flex items-center " + title}>Plans & Prices</h1>
                 <div></div>
             </NavBar>
             <div className="h-[4.7rem]"/>
