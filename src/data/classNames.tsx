c3219226c591ab951bf061204e2cb2d893ddc9a0
LUCHER4321 <lucher4321@gmail.com>
2025-07-14 11:31:24 -0400

    Update classNames.tsx

--- src/data/classNames.tsx
+++ src/data/classNames.tsx
@@ -1,6 +1,6 @@
 export const filledButton = "dark:text-black text-white rounded-full! bg-[#1B5E20]! dark:bg-[#D8EDD9]! hover:bg-black! dark:hover:bg-white!";
 export const borderButton = "text-black dark:text-white rounded-full! border-[#1B5E20]! dark:border-[#D8EDD9]! hover:bg-[#1B5E20]/100! dark:hover:bg-[#D8EDD9]/100! bg-black/0! hover:text-white dark:hover:text-black";
-export const blankButton = "text-black dark:text-white bg-black/0! rounded-full! border-black/0! px-2!"
+export const blankButton = "text-black dark:text-white bg-black/0! rounded-full! border-black/0! px-2! focus:outline-0! focus-visible:outline-0!"
 export const title = "font-bold text-[#1B5E20]! dark:text-[#D8EDD9]!"
 export const card = "rounded text-center duration-300 hover:-translate-y-[10px] shadow-lg bg-neutral-100 dark:bg-neutral-800";
 export const aText = "text-[#1B5E20]! dark:text-[#D8EDD9]! hover:underline! cursor-pointer"
