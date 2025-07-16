58c9eee7bdcaa1b50e5cb9425d620b8028c9f2df
LUCHER4321 <lucher4321@gmail.com>
2025-07-15 22:02:29 -0400

    Update SideFooter.tsx

--- src/components/dashboard/SideFooter.tsx
+++ src/components/dashboard/SideFooter.tsx
@@ -23,8 +23,8 @@ export const SideFooter = ({
             <div className={`flex flex-row ${expanded ? "justify-between" : "justify-center"} items-center dark:text-[#D8EDD9] text-[#1B5E20]`}>
                 {photo ? <img src={photo} className={photoClass} alt="Profile"/> : <i className={photoClass + " fas fa-user dark:text-[#D8EDD9] text-[#1B5E20]"}/>}
                 {expanded && <div className="flex flex-col">
-                    {username && <p className="block font-medium">@{username}</p>}
-                    {plan && <p className="block text-xs">{plans.get(plan)?.name} Plan</p>}
+                    {username && <p className="block font-medium line-clamp-1">@{username}</p>}
+                    {plan && <p className="block text-xs line-clamp-1">{plans.get(plan)?.name} Plan</p>}
                 </div>}
                 {expanded && <a href={href} className="dark:text-[#D8EDD9]! text-[#1B5E20]!">
                     <i className="fas fa-sign-out-alt"/>
