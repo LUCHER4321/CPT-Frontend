dd15a651ee841ffa6cfa803b0bfe6d245b5c5862
LUCHER4321 <lucher4321@gmail.com>
2025-07-15 21:55:14 -0400

    Create dashboardItems.tsx

---
+++ src/data/dashboardItems.tsx
@@ -0,0 +1,27 @@
+export const dashboardItems = [
+    {
+        href: "/",
+        icon: "fa-home",
+        name: "Home"
+    },
+    {
+        href: "/trees",
+        icon: "fa-project-diagram",
+        name: "My Trees"
+    },
+    {
+        href: "/liked",
+        icon: "fa-heart",
+        name: "Liked Trees"
+    },
+    {
+        href: "/collabs",
+        icon: "fa-users",
+        name: "Collaborations"
+    },
+    {
+        href: "/settings",
+        icon: "fa-cog",
+        name: "Settings"
+    }
+];
