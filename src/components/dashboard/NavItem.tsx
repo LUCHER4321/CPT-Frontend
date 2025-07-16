45c765a49628ec5bf4d6a5ba196fd6a7da607578
LUCHER4321 <lucher4321@gmail.com>
2025-07-15 21:55:43 -0400

    Create NavItem.tsx

---
+++ src/components/dashboard/NavItem.tsx
@@ -0,0 +1,25 @@
+interface NavItemProps {
+    href?: string;
+    icon?: string;
+    children?: any;
+    expanded?: boolean;
+    current?: boolean;
+}
+
+export const NavItem = ({
+    href,
+    icon,
+    children,
+    expanded,
+    current
+}: NavItemProps) => {
+    return (
+        <li className={`my-1 px-6 py-3 relative ${current ? "bg-[#1B5E20] dark:bg-[#D8EDD9]" : ""}`}>
+            <a href={href} className={current ? "text-[#D8EDD9]! dark:text-[#1B5E20]!" : "dark:text-[#D8EDD9]! text-[#1B5E20]!"}>
+                <i className={`${expanded ? "mr-4" : ""} fas ${icon} w-5`}/>
+                {expanded && children}
+            </a>
+            {current && <div className="absolute top-0 bottom-0 left-0 w-1 bg-[#D8EDD9] dark:bg-[#1B5E20]"/>}
+        </li>
+    )
+};
