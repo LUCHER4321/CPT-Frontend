3eea32aa4ac3965bfee31a26f5fd56cf2a280423
LUCHER4321 <lucher4321@gmail.com>
2025-07-15 22:02:26 -0400

    Update NavItem.tsx

--- src/components/dashboard/NavItem.tsx
+++ src/components/dashboard/NavItem.tsx
@@ -15,7 +15,7 @@ export const NavItem = ({
 }: NavItemProps) => {
     return (
         <li className={`my-1 px-6 py-3 relative ${current ? "bg-[#1B5E20] dark:bg-[#D8EDD9]" : ""}`}>
-            <a href={href} className={current ? "text-[#D8EDD9]! dark:text-[#1B5E20]!" : "dark:text-[#D8EDD9]! text-[#1B5E20]!"}>
+            <a href={href} className={`line-clamp-1 ${current ? "text-[#D8EDD9]! dark:text-[#1B5E20]!" : "dark:text-[#D8EDD9]! text-[#1B5E20]!"}`}>
                 <i className={`${expanded ? "mr-4" : ""} fas ${icon} w-5`}/>
                 {expanded && children}
             </a>
