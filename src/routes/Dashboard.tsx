696cb40f3e8aa1b24f8b10a88e949bd23a614e0c
LUCHER4321 <lucher4321@gmail.com>
2025-07-16 22:10:25 -0400

    Update Dashboard.tsx

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -3,10 +3,12 @@ import { dashboardItems } from "../data/dashboardItems";
 import { Sidebar } from "../components/dashboard/Sidebar";
 import { exampleUsers } from "../data/example";
 import { getMe } from "../api/user";
+import { Header } from "../components/dashboard/Header";
 
 export const Dashboard = () => {
     const [expanded, setExpanded] = useState(false);
     const [user, setUser] = useState(exampleUsers[0]);
+    const [search, setSearch] = useState("");
     useEffect(() => {
         document.title = "Life Tree | Dashboard";
         getMe({}).then(u => setUser(u ?? exampleUsers[0]));
@@ -15,12 +17,18 @@ export const Dashboard = () => {
         <div className="size-full flex flex-row justify-start">
             <Sidebar
                 expanded={expanded}
+                setExpanded={setExpanded}
                 items={dashboardItems}
                 currentPage="/"
                 user={user}
-                onMouseEnter={() => setExpanded(true)}
-                onMouseLeave={() => setExpanded(false)}
             />
+            <main className="flex flex-col w-full">
+                <Header
+                    search={search}
+                    setSearch={setSearch}
+                    count={10}
+                />
+            </main>
         </div>
     )
 };
