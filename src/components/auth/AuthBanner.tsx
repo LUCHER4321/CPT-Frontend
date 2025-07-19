ac7a041cb848b7d7dee719979eb09735addb1bee
LUCHER4321 <lucher4321@gmail.com>
2025-07-19 15:32:22 -0400

    Update AuthBanner.tsx

--- src/components/auth/AuthBanner.tsx
+++ src/components/auth/AuthBanner.tsx
@@ -7,8 +7,8 @@ export const AuthBanner = () => {
                 <h3 className={"text-[1.8rem] mb-[1rem] " + title}>Explore the tree of life</h3>
                 <p className="mb-8">Create, share and collaborate in phylogenetic trees with the community</p>
             </div>
-            <img src="/transparentLogo.svg" alt="Logo" className="max-h-70! hidden dark:flex"/>
-            <img src="/transparentLogoLight.svg" alt="Logo" className="max-h-70! flex dark:hidden"/>
+            <img src="/logo.svg" alt="Logo" className="max-h-70! hidden dark:flex"/>
+            <img src="/logoLight.svg" alt="Logo" className="max-h-70! flex dark:hidden"/>
         </div>
     )
 }
