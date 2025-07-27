835ac9c804bef0568631d1ddc214b383ef2ca078
LUCHER4321 <lucher4321@gmail.com>
2025-07-27 19:05:07 -0400

    Update Dashboard.tsx

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -4,17 +4,49 @@ import { Sidebar } from "../components/dashboard/Sidebar";
 import { exampleUsers } from "../data/example";
 import { getMe } from "../api/user";
 import { Header } from "../components/dashboard/Header";
+import { myTreesCount } from "../api/phTree";
+import { getNotifications } from "../api/notification";
+import type { NotificationResponse } from "../types";
+import { NotificationWS } from "../classes/NotificationWS";
+import { socket } from "../api/socket";
+import { TreesData } from "../components/dashboard/TreesData";
 
 export const Dashboard = () => {
     const [expanded, setExpanded] = useState(false);
     const [user, setUser] = useState(exampleUsers[0]);
+    const [trees, setTrees] = useState<number | undefined>(undefined);
+    const [myTrees, setMyTrees] = useState<number | undefined>(undefined);
+    const [collabs, setCollabs] = useState<number | undefined>(undefined);
+    const [notifications, setNotifications] = useState<NotificationResponse[]>([]);
     const [search, setSearch] = useState("");
+    const [_, setNotificationWS] = useState<NotificationWS | undefined>(undefined)
     useEffect(() => {
         document.title = "Life Tree | Dashboard";
-        getMe({}).then(u => setUser(u ?? exampleUsers[0]));
+        getMe({}).then(u => {
+            setUser(u ?? exampleUsers[0]);
+            myTreesCount({}).then(t => {
+                if(t) {
+                    setTrees(t.total);
+                    setMyTrees(t.myTrees);
+                    setCollabs(t.collabs);
+                }
+            });
+            getNotifications({
+                from: new Date(Date.now() - 1000 * 60 * 60 * 24 * 365.25), // 1 YO
+                to: new Date(),
+                limit: 10
+            }).then(n => {
+                setNotifications(n ?? []);
+            });
+            setNotificationWS(new NotificationWS({
+                socket,
+                response: nr => setNotifications(prev => prev.concat([nr])),
+                userId: u?.id ?? ""
+            }))
+        });
     }, []);
     return (
-        <div className="size-full flex flex-row justify-start">
+        <div className="size-full flex flex-col-reverse sm:flex-row justify-between h-screen sm:justify-start">
             <Sidebar
                 expanded={expanded}
                 setExpanded={setExpanded}
@@ -26,8 +58,15 @@ export const Dashboard = () => {
                 <Header
                     search={search}
                     setSearch={setSearch}
-                    count={10}
+                    count={notifications.map(n => !n.seen).length}
                 />
+                <div className="w-full h-full content-center grid grid-cols-1 sm:grid-cols-3 gap-5 sm:px-10 sm:gap-10 overflow-y-scroll">
+                    <TreesData
+                        trees={trees}
+                        myTrees={myTrees}
+                        collabs={collabs}
+                    />
+                </div>
             </main>
         </div>
     )
