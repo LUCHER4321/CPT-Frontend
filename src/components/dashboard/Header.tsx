32ba9622fda811dc87a4ea4f328b4f1a6ffb619f
LUCHER4321 <lucher4321@gmail.com>
2025-07-27 19:10:26 -0400

    Update Header.tsx

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -20,9 +20,9 @@ export const Header = ({
     return (
         <header className="p-6 flex flex-col sm:flex-row justify-between items-center w-full shadow-lg">
             <h1 className={"block text-[2em]! font-bold " + title}>Welcome back</h1>
-            <div className="flex flex-row justify-between">
+            <div className="flex flex-row-reverse sm:flex-row justify-between w-full sm:w-auto pt-5 sm:pt-0">
                 <Notifications
-                    className="flex items-center p-2! bg-black/0! border-black/0! focus:outline-0! focus-visible:outline-0! mr-6"
+                    className="flex items-center p-0! sm:p-2! bg-black/0! border-black/0! focus:outline-0! focus-visible:outline-0! mr-6"
                     count={count}
                     active={active}
                     setActive={setActive}
