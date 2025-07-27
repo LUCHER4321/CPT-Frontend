4c56ccce6dd19aacda2bafca9e241c7f19c9af4e
LUCHER4321 <lucher4321@gmail.com>
2025-07-27 19:04:34 -0400

    Update SideHeader.tsx

--- src/components/dashboard/SideHeader.tsx
+++ src/components/dashboard/SideHeader.tsx
@@ -2,13 +2,15 @@ import { title } from "../../data/classNames";
 
 interface SideHeaderProps{
     expanded?: boolean;
+    className?: string;
 }
 
 export const SideHeader = ({
-    expanded
+    expanded,
+    className
 }: SideHeaderProps) => {
     return (
-        <div className="px-4 py-6 border-b">
+        <div className={"px-4 py-6 border-b " + className}>
             <a href="/" className={`text-xl line-clamp-1 ${expanded ? "" : "invisible"} ${title}`}>Life Tree</a>
         </div>
     )
