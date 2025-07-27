bfe95de031376bcca613b08ecb2cab105adc1f37
LUCHER4321 <lucher4321@gmail.com>
2025-07-27 19:05:04 -0400

    Create TreesData.tsx

---
+++ src/components/dashboard/TreesData.tsx
@@ -0,0 +1,48 @@
+import { title } from "../../data/classNames"
+import { Card } from "../home/Card"
+
+interface TreesDataProps {
+    trees?: number;
+    myTrees?: number;
+    collabs?: number;
+}
+
+export const TreesData = ({
+    trees,
+    myTrees,
+    collabs
+}: TreesDataProps) => {
+    return(
+        <>
+            <h2 className={"col-span-full text-[2rem] mb-2 " + title}>Trees Data</h2>
+            {(trees ?? 0) > 0 ? <>
+                <Card
+                    fa="fa-project-diagram"
+                    title={trees?.toString()}
+                    description={trees ? "Total Trees" : "New Tree"}
+                    href="/account/trees"
+                />
+                <Card
+                    fa="fa-user"
+                    title={myTrees?.toString()}
+                    description={myTrees ? "My Trees" : "New Tree"}
+                    href="/account/trees/me"
+                />
+                <Card
+                    fa="fa-users"
+                    title={collabs?.toString()}
+                    description={collabs ? "Collaborations" : "New Tree"}
+                    href="/account/collabs"
+                />
+            </> : <>
+                <div/>
+                <Card
+                    fa="fa-plus"
+                    description="New Tree"
+                    href="/account/trees"
+                />
+                <div/>
+            </>}
+        </>
+    )
+}
