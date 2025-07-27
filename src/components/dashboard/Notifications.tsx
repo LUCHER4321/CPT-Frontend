622fe804abf28eba19f6334aab86e0e6f8f0a56b
LUCHER4321 <lucher4321@gmail.com>
2025-07-27 19:04:49 -0400

    Update Notifications.tsx

--- src/components/dashboard/Notifications.tsx
+++ src/components/dashboard/Notifications.tsx
@@ -1,17 +1,24 @@
 interface NotificationsProps {
     className?: string;
     count?: number;
+    children?: any;
+    active?: boolean;
+    setActive?: (b: boolean) => void;
 }
 
 export const Notifications = ({
     className,
-    count
+    count,
+    children,
+    active,
+    setActive
 }: NotificationsProps) => {
     return (
-        <div className={className}>
+        <button className={className} onClick={() => setActive?.(!active)}>
             <i className="fas fa-bell relative">
                 {count ? <span className="absolute left-1/2 bottom-1/2 dark:bg-[#1B5E20] bg-[#D8EDD9] aspect-square min-w-6.5 rounded-full flex items-center justify-center text-xs p-1">{count > 9 ? "9+" : count}</span> : <></>}
+                {active && children}
             </i>
-        </div>
+        </button>
     )
 };
