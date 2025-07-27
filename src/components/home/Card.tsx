39e52333418e3c44adb09c3c0b161d086b60669c
LUCHER4321 <lucher4321@gmail.com>
2025-07-27 19:04:14 -0400

    Update Card.tsx

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -4,18 +4,20 @@ interface CardProps {
     fa?: string;
     title?: string;
     description?: string;
+    href?: string;
 }
 
 export const Card = ({
     fa,
     title,
-    description
+    description,
+    href
 }: CardProps) => {
     return (
-        <div className={"p-[2rem] " + card}>
+        <a className={"p-[2rem] " + card} href={href}>
             <i className={"text-[2.5rem] mb-[1rem] fas " + fa + " " + titleClass}/>
             <h3 className={"mb-4 " + titleClass}>{title}</h3>
-            <p>{description}</p>
-        </div>
+            <p className="text-[#1B5E20]! dark:text-[#D8EDD9]!">{description}</p>
+        </a>
     );
 };
