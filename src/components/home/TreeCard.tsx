eff65451df616dbbf15bafc364ff137e1f3752d1
LUCHER4321 <lucher4321@gmail.com>
2025-07-28 19:28:27 -0400

    Update TreeCard.tsx

--- src/components/home/TreeCard.tsx
+++ src/components/home/TreeCard.tsx
@@ -6,23 +6,30 @@ interface TreeCardProps {
 }
 
 export const TreeCard = ({
-    tree
+    tree: {
+        id,
+        image,
+        name,
+        username,
+        likes,
+        comments
+    }
 }: TreeCardProps) => {
     return (
-        <div className={card}>
-            <img src={tree.image}/>
-            <h3 className={"text-start px-4 pt-4 text-[1.17em] " + title}>{tree.name}</h3>
-            <p className="text-start px-4 ">By: @{tree.username}</p>
+        <a className={card + " " + title} href={`/trees/${id}`}>
+            <img src={image}/>
+            <h3 className={"text-start px-4 pt-4 text-[1.17em] " + title}>{name}</h3>
+            <p className="text-start px-4 ">By: @{username}</p>
             <div className="flex justify-between flex-row w-full p-4">
                 <div className="flex flex-row items-center space-x-1">
                     <i className="fas fa-heart"/>
-                    <p>{tree.likes}</p>
+                    <p>{likes}</p>
                 </div>
                 <div className="flex flex-row items-center space-x-1">
                     <i className="fas fa-comment"/>
-                    <p>{tree.comments}</p>
+                    <p>{comments}</p>
                 </div>
             </div>
-        </div>
+        </a>
     )
 };
