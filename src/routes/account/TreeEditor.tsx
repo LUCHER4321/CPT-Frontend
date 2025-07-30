f3a20e9ce41d79d3986a9a8fc7103a7913008389
LUCHER4321 <lucher4321@gmail.com>
2025-07-30 17:46:02 -0400

    Update TreeEditor.tsx

--- src/routes/account/TreeEditor.tsx
+++ src/routes/account/TreeEditor.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import type { NotificationResponse, PhTreeResponse, UserResponse } from "../../types";
 import { getTree } from "../../api/phTree";
 import { getMe } from "../../api/user";
@@ -11,6 +11,9 @@ import { dashboardItems } from "../../data/dashboardItems";
 import { NotificationWS } from "../../classes/NotificationWS";
 import { socket } from "../../api/socket";
 import { PhTreeWS } from "../../classes/PhTreeWS";
+import { TreeChange, TreeProp } from "../../enums";
+import { TreeVisualization } from "../../components/dashboard/trees/TreeVisualization";
+import { TreeProperties } from "../../components/dashboard/trees/TreeProperties";
 
 export const TreeEditor = () => {
     const [expanded, setExpanded] = useState(false);
@@ -21,8 +24,11 @@ export const TreeEditor = () => {
     const [active, setActive] = useState(false);
     const [name, setName] = useState("");
     const [_, setNotificationWS] = useState<NotificationWS | undefined>(undefined);
-    const [__, setPhTreeWS] = useState<PhTreeWS | undefined>(undefined)
+    const [__, setPhTreeWS] = useState<PhTreeWS | undefined>(undefined);
+    const [chronoScale, setChronoScale] = useState(true);
+    const [prop, setProp] = useState(TreeProp.TREE);
     const { id } = useParams();
+    const history = useNavigate();
     useEffect(() => {
         if(id) getMe({}).then(u => {
             setUser(u);
@@ -32,9 +38,28 @@ export const TreeEditor = () => {
                     setName(t.name);
                     setPhTreeWS(new PhTreeWS({
                         socket,
-                        response: () => {},
+                        response: ({change, phTree, species}) => {
+                            switch (change) {
+                                case TreeChange.NEW:
+                                    if(!species) return;
+                                    break;
+                                case TreeChange.EDIT:
+                                    if(!species) return;
+                                    break;
+                                case TreeChange.DELETE:
+                                    if(!species) return;
+                                    break;
+                                case TreeChange.TREE:
+                                    if(!phTree) return;
+                                    break;
+                            }
+                        },
                         treeId: t.id
-                    }))
+                    }));
+                    if(u?.id && ![...(t.collaborators ?? []), t.userId].includes(u.id)) {
+                        if(u) history("/account");
+                        else history("/");
+                    };
                 }
             });
             getNotifications({}).then(n => {
@@ -77,6 +102,20 @@ export const TreeEditor = () => {
                     />
                 </div>
             </Header>
+            <div className="flex flex-row size-full overflow-hidden">
+                <TreeVisualization
+                    chronoScale={chronoScale}
+                    setChronoScale={setChronoScale}
+                >
+                    TreeVisualization
+                </TreeVisualization>
+                <TreeProperties
+                    prop={prop}
+                    setProp={setProp}
+                >
+                    TreeProperties
+                </TreeProperties>
+            </div>
         </main>
     </div>)
 }
