bf92a06d2c0b3bf796c8a2ab83d2b8d07321dba5
LUCHER4321 <lucher4321@gmail.com>
2025-07-30 10:59:34 -0400

    Update Header.tsx

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,4 +1,3 @@
-import { title } from "../../data/classNames";
 import { Notifications } from "./Notifications";
 import { SearchInput } from "./SearchInput";
 
@@ -8,6 +7,7 @@ interface HeaderProps {
     count?: number;
     active?: boolean;
     setActive?: (b: boolean) => void;
+    children?: any;
 }
 
 export const Header = ({
@@ -15,11 +15,12 @@ export const Header = ({
     setSearch,
     count,
     active,
-    setActive
+    setActive,
+    children
 }: HeaderProps) => {
     return (
         <header className="p-6 flex flex-col sm:flex-row justify-between items-center w-full shadow-lg">
-            <h1 className={"block text-[2em]! font-bold " + title}>Welcome back</h1>
+            {children}
             <div className="flex flex-row-reverse sm:flex-row justify-between w-full sm:w-auto pt-5 sm:pt-0">
                 <Notifications
                     className="flex items-center p-0! sm:p-2! bg-black/0! border-black/0! focus:outline-0! focus-visible:outline-0! mr-6"
