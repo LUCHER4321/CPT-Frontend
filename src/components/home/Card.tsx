8f20e483a4b03e261ddac471560c89fe3663ed9e
LUCHER4321 <lucher4321@gmail.com>
2025-07-30 11:00:09 -0400

    Update Card.tsx

--- src/components/home/Card.tsx
+++ src/components/home/Card.tsx
@@ -6,6 +6,7 @@ interface CardProps {
     description?: string;
     href?: string;
     image?: string;
+    onClick?: () => void;
     children?: any;
 }
 
@@ -15,14 +16,24 @@ export const Card = ({
     description,
     href,
     image,
+    onClick,
     children
 }: CardProps) => {
-    return (
-        <a className={"p-[2rem] relative " + card} href={href}>
+    const inside = (
+        <>
             {image ? <img className="h-10! rounded-full" src={image}/> : <i className={"text-[2.5rem] mb-[1rem] fas " + fa + " " + titleClass}/>}
             <h3 className={"mb-4 " + titleClass}>{title}</h3>
             <p className="text-[#1B5E20]! dark:text-[#D8EDD9]!">{description}</p>
             {children}
-        </a>
+        </>
+    )
+    return (
+        <>
+            {href ? <a className={"p-[2rem] relative " + card} href={href} onClick={onClick}>
+                {inside}
+            </a> : <button className={"p-[2rem] relative border-black/0! focus:outline-0! focus-visible:outline-0! " + card} onClick={onClick}>
+                {inside}
+            </button>}
+        </>
     );
 };
