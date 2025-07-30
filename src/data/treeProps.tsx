232f06ac293cdcbe768554d134e77b2d95721cd7
LUCHER4321 <lucher4321@gmail.com>
2025-07-30 17:46:07 -0400

    Create treeProps.tsx

---
+++ src/data/treeProps.tsx
@@ -0,0 +1,16 @@
+import { TreeProp } from "../enums";
+
+export const treeProps = new Map([
+    [TreeProp.TREE, {
+            icon: "fa-tree",
+            name: "Tree"
+    }],
+    [TreeProp.NODE, {
+            icon: "fa-circle",
+            name: "Node"
+    }],
+    [TreeProp.COLLABORATORS, {
+            icon: "fa-users",
+            name: "Collaborators"
+    }]
+])
