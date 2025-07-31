72ad92928d3e10a482272e77e93cdf9054d805cc
LUCHER4321 <lucher4321@gmail.com>
2025-07-31 16:40:15 -0400

    Create NodeProps.tsx

---
+++ src/components/dashboard/trees/NodeProps.tsx
@@ -0,0 +1,149 @@
+import type { PhTreeResponse } from "../../../types";
+import { AuthField } from "../../auth/AuthField";
+import { updateSpecies } from "../../../api/species";
+import type { PhTreeWS } from "../../../classes/PhTreeWS";
+import { TimeUnit, TreeChange } from "../../../enums";
+import { Species, type SpeciesJSON } from "chrono-phylo-tree";
+import { nullableInput } from "../../../utils/nullableInput";
+import { IconInput } from "../../IconInput";
+import { ImageProp } from "./ImageProp";
+import type { DropzoneInputProps, DropzoneRootProps } from "react-dropzone";
+
+interface NodePropsProps {
+    tree?: PhTreeResponse;
+    treeSocket?: PhTreeWS;
+    species?: Species;
+    setSpecies?: (s: Species) => void;
+    unit?: TimeUnit;
+    setUnit?: (tu: TimeUnit) => void;
+    getRootProps?: (props?: DropzoneRootProps) => DropzoneRootProps;
+    getInputProps?: (props?: DropzoneInputProps) => DropzoneInputProps;
+    isDragActive?: boolean;
+}
+
+export const NodeProps = ({
+    tree,
+    treeSocket,
+    species,
+    setSpecies,
+    unit = TimeUnit.Y,
+    setUnit,
+    getRootProps,
+    getInputProps,
+    isDragActive
+}: NodePropsProps) => {
+    const updateProp = <T,>(change: (t: T) => void, data: (t: T) => {
+        ancestorId?: string | null;
+    } & Partial<Omit<SpeciesJSON, "image">>) => (t: T) => {
+        if(species?.id && tree) {
+            change(t);
+            setSpecies?.(species);
+            updateSpecies({
+                treeId: tree.id,
+                id: species.id.toString(),
+                ...data
+            }).then(sp => treeSocket?.emit({
+                type: TreeChange.EDIT,
+                species: sp
+            }));
+        }
+    }
+    return (<>
+        <AuthField
+            name="Species' name"
+            type="text"
+            placeholder="Name this species..."
+            value={species?.name}
+            setValue={updateProp(n => {
+                if(species) species.name = n;
+            }, n => ({
+                name: n
+            }))}
+        />
+        <AuthField
+            name="Ancestor"
+            selected={species?.ancestor}
+            options={[undefined, ...species?.firstAncestor(true).allDescendants(false).filter(sp => sp.display && sp.id !== species.id) ?? []]}
+            optionsDisplay={sp => sp?.name ?? "None (root)"}
+            setSelected={updateProp(sp => {
+                if(species) species.ancestor = sp;
+            }, sp => ({
+                ancestorId: sp?.id?.toString() ?? null
+            }))}
+        />
+        <AuthField
+            name="Time Unit"
+            selected={unit}
+            setSelected={setUnit}
+            options={[TimeUnit.Y, TimeUnit.KY, TimeUnit.MY, TimeUnit.BY, TimeUnit.TY]}
+            optionsDisplay={tu => {
+                switch(tu) {
+                    case TimeUnit.Y: return "Years"
+                    case TimeUnit.KY: return "Thousand Years"
+                    case TimeUnit.MY: return "Million Years"
+                    case TimeUnit.BY: return "Billion Years"
+                    case TimeUnit.TY: return "Trillion Years"
+                }
+            }}
+        />
+        <AuthField
+            name="Apparition"
+            type="number"
+            placeholder="When did this species appear?..."
+            value={nullableInput(species?.apparition, a => (a / unit).toString())}
+            min={species?.ancestor?.apparition}
+            max={species?.ancestor?.extinction()}
+            setValue={updateProp(a => {
+                if(species) species.apparition = +a
+            }, a => ({
+                apparition: species?.ancestor ? undefined : +a,
+                afterApparition: species?.ancestor ? +a - species.ancestor.apparition : undefined
+            }))}
+        >
+            <IconInput
+                type="range"
+                className="w-full"
+                value={nullableInput(species?.apparition, a => (a / unit).toString())}
+                min={species?.ancestor?.apparition}
+                max={species?.ancestor?.extinction()}
+                setValue={updateProp(a => {
+                    if(species) species.apparition = +a
+                }, a => ({
+                    apparition: species?.ancestor ? undefined : +a,
+                    afterApparition: species?.ancestor ? +a - species.ancestor.apparition : undefined
+                }))}
+            />
+        </AuthField>
+        <AuthField
+            name="Duration"
+            type="number"
+            placeholder="How much time did this species exist?..."
+            value={nullableInput(species?.duration, a => (a / unit).toString())}
+            min={species?.allDescendants(false).filter(s => s !== species).map(s => s.apparition - species.apparition).reduce((a, b) => a > b ? a : b) ?? 0}
+            setValue={updateProp(d => {
+                if(species) species.duration = +d;
+            }, d => ({
+                duration: +d
+            }))}
+        />
+        <AuthField
+            name="Description"
+            type="text"
+            placeholder="Describe this species"
+            value={species?.description}
+            setValue={updateProp(d => {
+                if(species) species.description = d;
+            }, d => ({
+                description: d
+            }))}
+            textArea
+        />
+        <ImageProp
+            title="Species' Image"
+            image={species?.image}
+            getRootProps={getRootProps}
+            getInputProps={getInputProps}
+            isDragActive={isDragActive}
+        />
+    </>)
+};
