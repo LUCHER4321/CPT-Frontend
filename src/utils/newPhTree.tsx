d8c4fc3187dc101d96aa77f8fceeaa3c24583777
LUCHER4321 <lucher4321@gmail.com>
2025-08-02 14:38:58 -0400

    Update newPhTree.tsx

--- src/utils/newPhTree.tsx
+++ src/utils/newPhTree.tsx
@@ -1,26 +1,17 @@
 import { useNavigate } from "react-router-dom";
-import { createTree, getMyTrees, myTreesCount } from "../api/phTree";
+import { createTree, myTreesCount } from "../api/phTree";
 import { getMe } from "../api/user";
 import { plans } from "../data/prices";
-import { nullableInput } from "./nullableInput";
 
-export const newPhTree = async (defaultName = "Untitled Phylogenetic Tree") => {
+export const newPhTree = (history = useNavigate()) => async () => {
     const { myTrees } = await myTreesCount({}) ?? {};
     const { plan } = await getMe({}) ?? {};
-    if((myTrees ?? 0) >= (nullableInput(plan, plans.get)?.constraints.maxTrees ?? Number.MAX_SAFE_INTEGER)) throw new Error("Maximum number of Phylogenetic Trees")
-    const history = useNavigate();
-    let name = defaultName;
-    let n = 0;
-    if((await getMyTrees({ search: defaultName }))?.map(t => t.name).includes(defaultName)) {
-        do{
-            n++;
-            name = `${defaultName} (${n})`;
-        }
-        while((await getMyTrees({ search: name }))?.map(t => t.name).includes(name));
-    }
+    if(!plan || myTrees === undefined) return console.log({ plan, myTrees });
+    if(myTrees >= (plans.get(plan)?.constraints.maxTrees ?? Number.MAX_SAFE_INTEGER)) throw new Error("Maximum number of Phylogenetic Trees");
     const { id } = await createTree({
-        name,
+        name: "Untitled Phylogenetic Tree",
         isPublic: false
     }) ?? {};
-    await history(`/trees/${id}`);
+    console.log({ id })
+    if(id) await history(`/account/trees/${id}`);
 }
