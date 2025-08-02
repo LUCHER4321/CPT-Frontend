cd2934eb391d3d1570e2c0eff697dfd7983dbc28
LUCHER4321 <lucher4321@gmail.com>
2025-08-02 14:39:35 -0400

    Update ImageProp.tsx

--- src/components/dashboard/trees/ImageProp.tsx
+++ src/components/dashboard/trees/ImageProp.tsx
@@ -1,4 +1,5 @@
 import type { DropzoneInputProps, DropzoneRootProps } from "react-dropzone";
+import { circleImage } from "../../../data/classNames";
 
 interface ImagePropProps {
     title?: string;
@@ -18,7 +19,7 @@ export const ImageProp = ({
     return (
         <div className="mb-5 flex flex-col">
             <p className="font-bold mb-2">{title}</p>
-            {image && <img src={image} className="h-70! w-70! self-center object-cover rounded-full my-5" />}
+            {image && <img src={image} className={"h-70! w-70! self-center my-5 " + circleImage} />}
             <div { ...getRootProps?.({ className: `border border-dashed rounded transition-all duration-300 ${isDragActive ? "text-[#D8EDD9] dark:text-[#1B5E20] bg-[#1B5E20] dark:bg-[#D8EDD9]" : ""} hover:text-[#D8EDD9] dark:hover:text-[#1B5E20] hover:bg-[#1B5E20] dark:hover:bg-[#D8EDD9] text-xl justify-center p-10 cursor-pointer flex flex-row space-x-2 items-center` }) }>
                 <input { ...getInputProps?.() }/>
                 <i className="fas fa-upload"/>
