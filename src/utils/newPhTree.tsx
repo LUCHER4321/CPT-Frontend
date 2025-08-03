ff8e9ae467e65b5bcd30660f6b666d7e5b5081bc
LUCHER4321 <lucher4321@gmail.com>
2025-08-03 16:12:19 -0400

    Update newPhTree.tsx

--- src/utils/newPhTree.tsx
+++ src/utils/newPhTree.tsx
@@ -6,12 +6,11 @@ import { plans } from "../data/prices";
 export const newPhTree = (history = useNavigate()) => async () => {
     const { myTrees } = await myTreesCount({}) ?? {};
     const { plan } = await getMe({}) ?? {};
-    if(!plan || myTrees === undefined) return console.log({ plan, myTrees });
+    if(!plan || myTrees === undefined) return;
     if(myTrees >= (plans.get(plan)?.constraints.maxTrees ?? Number.MAX_SAFE_INTEGER)) throw new Error("Maximum number of Phylogenetic Trees");
     const { id } = await createTree({
         name: "Untitled Phylogenetic Tree",
         isPublic: false
     }) ?? {};
-    console.log({ id })
     if(id) await history(`/account/trees/${id}`);
 }
