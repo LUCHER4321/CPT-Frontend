3ae79589b45d33955a72f8bb5edf81ed734c74e9
LUCHER4321 <lucher4321@gmail.com>
2025-08-11 21:02:28 -0400

    Update LastTrees.tsx

--- src/components/dashboard/LastTrees.tsx
+++ src/components/dashboard/LastTrees.tsx
@@ -1,9 +1,9 @@
 import { title } from "../../data/classNames";
 import type { PhTreeResponse } from "../../types";
-import { Card } from "../home/Card";
+import { TreeCard } from "../home/TreeCard";
 
 interface LastTreesProps {
-    lastTrees?: PhTreeResponse[]
+    lastTrees?: (PhTreeResponse & { username: string })[]
 }
 
 export const LastTrees = ({
@@ -12,35 +12,12 @@ export const LastTrees = ({
     return (
         <>
             <h2 className={"col-span-full text-[2rem] mb-2 " + title}>Last Trees</h2>
-            {lastTrees.map(({
-                id,
-                name,
-                description,
-                image,
-                isPublic,
-                likes,
-                comments
-            }, index) => (
-                <Card
+            {lastTrees.map((tree, index) => (
+                <TreeCard
+                    tree={tree}
                     key={index}
-                    fa={"fa-project-diagram"}
-                    title={name}
-                    description={description}
-                    href={`/account/trees/${id}`}
-                    image={image}
-                >
-                    <i className={"absolute text-[1.5rem] right-[0.5rem] top-[0.5rem] fas " + title + " " + (isPublic ? "fa-globe" : "fa-lock")}/>
-                    {isPublic && <div className="flex flex-row w-full justify-between">
-                        <div className={"flex flex-row items-center space-x-1 " + title}>
-                            <i className="fas fa-heart"/>
-                            <p>{likes}</p>
-                        </div>
-                        <div className={"flex flex-row items-center space-x-1 " + title}>
-                            <i className="fas fa-comment"/>
-                            <p>{comments}</p>
-                        </div>
-                    </div>}
-                </Card>
+                    edit
+                />
             ))}
         </>
     )
