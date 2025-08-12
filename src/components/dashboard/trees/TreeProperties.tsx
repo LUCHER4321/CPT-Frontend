939e231544e11e2044571384f86a899330f564c3
LUCHER4321 <lucher4321@gmail.com>
2025-08-11 23:52:37 -0400

    Update TreeProperties.tsx

--- src/components/dashboard/trees/TreeProperties.tsx
+++ src/components/dashboard/trees/TreeProperties.tsx
@@ -6,17 +6,19 @@ interface TreePropertiesProps {
     children?: any;
     prop?: TreeProp;
     setProp?: (p: TreeProp) => void;
+    view?: boolean;
 }
 
 export const TreeProperties = ({
     children,
     prop,
-    setProp
+    setProp,
+    view
 }: TreePropertiesProps) => {
     return (
         <div className="min-w-88 max-w-88 h-full m-2 rounded shadow-lg flex flex-col overflow-hidden">
-            <div className="grid grid-cols-3 border-b">
-                {[...treeProps.entries()].map(([k, { icon, name }], index) => (
+            <div className={"grid border-b " + (view ? "grid-cols-2" : "grid-cols-3")}>
+                {[...treeProps.entries()].filter(([k]) => !view || k !== TreeProp.COLLABORATORS).map(([k, { icon, name }], index) => (
                     <button
                         className={`flex flex-col space-y-2.5 rounded-none! bg-black/0! transition-all! text-xs! duration-300 ${unborder} ${k === prop ? "text-[#1B5E20]! dark:text-[#D8EDD9]! border-b-2! border-[#1B5E20]! dark:border-[#D8EDD9]!" : ""}`}
                         key={index}
