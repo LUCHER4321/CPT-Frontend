8b0ad57dbf61ac9a91dbcf130ed3d69ab93e9a39
LUCHER4321 <lucher4321@gmail.com>
2025-08-11 21:02:08 -0400

    Update capitalizeFirstLetter.tsx

--- src/utils/capitalizeFirstLetter.tsx
+++ src/utils/capitalizeFirstLetter.tsx
@@ -4,3 +4,9 @@ export const capitalizeFirstLetter = (s: string) => {
   }
   return s.charAt(0).toUpperCase() + s.slice(1);
 }
+
+export const upperSplit = (s: string) => s
+  .replace(/([A-Z][a-z])/g, " $1")
+  .replace(/([A-Z])([A-Z])/g, "$1 $2")
+  .replace(/([0-9])([A-Z])/g, "$1 $2")
+  .trim();
