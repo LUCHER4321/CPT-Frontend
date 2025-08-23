ee6c45f636df1b86dc86bbcf1e89acfe8d412bcd
LUCHER4321 <lucher4321@gmail.com>
2025-08-22 21:50:18 -0400

    Update SideFooter.tsx

--- src/components/dashboard/SideFooter.tsx
+++ src/components/dashboard/SideFooter.tsx
@@ -27,7 +27,9 @@ export const SideFooter = ({
     return (
         <div className={"border-t p-4 pb-8 " + className}>
             <div className={`flex flex-row ${expanded ? "justify-between" : "justify-center"} items-center space-x-[0.75rem] dark:text-[#D8EDD9] text-[#1B5E20]`}>
-                {photo ? <img src={photo} className={photoClass} alt="Profile"/> : <i className={photoClass + " fas fa-user dark:text-[#D8EDD9] text-[#1B5E20] text-center text-4xl"}/>}
+                <a href="/account/profile">
+                    {photo ? <img src={photo} className={photoClass} alt="Profile"/> : <i className={photoClass + " fas fa-user dark:text-[#D8EDD9] text-[#1B5E20] text-center text-4xl"}/>}
+                </a>
                 {expanded && <div className="flex flex-col">
                     {username && <p className="block font-medium line-clamp-1">@{username}</p>}
                     {plan && <p className="block text-xs line-clamp-1">{plans.get(plan)?.name} Plan</p>}
