1cc7e979ffe37a339950e21e67909ff97456a252
LUCHER4321 <lucher4321@gmail.com>
2025-08-23 21:03:14 -0400

    Update dateToString.tsx

--- src/utils/dateToString.tsx
+++ src/utils/dateToString.tsx
@@ -2,6 +2,15 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 
 const fixedNumber = (n: number, digits = 2) => n.toString().padStart(digits, "0")
 
+const nthDay = (d: number) => {
+    switch(d % 10) {
+        case 1: return d.toString() + (d === 11 ? "th" : "st");
+        case 2: return d.toString() + (d === 12 ? "th" : "nd");
+        case 3: return d.toString() + (d === 13 ? "th" : "rd");
+        default: return d.toString() + "th";
+    }
+}
+
 export const dateToString = (format?: string, date?: Date) => {
     const newDate = new Date(date ?? new Date());
     return format
@@ -9,6 +18,7 @@ export const dateToString = (format?: string, date?: Date) => {
         .replace("YY", fixedNumber(newDate.getFullYear() % 100))
         .replace("MMn", months.at(newDate.getMonth()) ?? "")
         .replace("MM", fixedNumber(newDate.getMonth() + 1))
+        .replace("DDth", nthDay(newDate.getDate()))
         .replace("DD", fixedNumber(newDate.getDate()))
         .replace("hh", fixedNumber(newDate.getHours()))
         .replace("mm", fixedNumber(newDate.getMinutes()))
