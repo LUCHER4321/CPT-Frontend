aa35ebb5c78e3c26d7cd50fbf0d01ff6b2db54a7
LUCHER4321 <lucher4321@gmail.com>
2025-08-24 19:58:53 -0400

    Update ToolButton.tsx

--- src/components/dashboard/trees/ToolButton.tsx
+++ src/components/dashboard/trees/ToolButton.tsx
@@ -5,16 +5,22 @@ interface ToolButtonProps {
     icon?: string;
     onClick?: () => void;
     enable?: boolean;
+    className?: string;
 }
 
 export const ToolButton = ({
     title,
     icon,
     onClick,
-    enable = true
+    enable = true,
+    className
 }: ToolButtonProps) => {
     return (
-        <button className={`size-9 rounded p-0! flex items-center justify-center text-[calc(40px/3)]! ${enable ? "bg-[#D8EDD9]! dark:bg-[#1B5E20]! text-[#1B5E20]! dark:text-[#D8EDD9]! hover:bg-[#1B5E20]! hover:text-[#D8EDD9]! hover:dark:bg-[#D8EDD9]! hover:dark:text-[#1B5E20]!" : "cursor-not-allowed!"} ${unborder}`} onClick={enable ? onClick : undefined} title={enable ? title : undefined}>
+        <button
+            className={`size-9 rounded p-0! flex items-center justify-center text-[calc(40px/3)]! ${enable ? "bg-[#D8EDD9]! dark:bg-[#1B5E20]! text-[#1B5E20]! dark:text-[#D8EDD9]! hover:bg-[#1B5E20]! hover:text-[#D8EDD9]! hover:dark:bg-[#D8EDD9]! hover:dark:text-[#1B5E20]!" : "cursor-not-allowed!"} ${unborder} ${className}`}
+            onClick={enable ? onClick : undefined}
+            title={enable ? title : undefined}
+        >
             <i className={"fas " + icon}/>
         </button>
     )
