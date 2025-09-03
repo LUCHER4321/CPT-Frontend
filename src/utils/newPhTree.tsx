8f5040c1aaffdd964446384a7427c3174e3812a8
LUCHER4321 <lucher4321@gmail.com>
2025-09-03 19:36:21 -0400

    Update newPhTree.tsx

--- src/utils/newPhTree.tsx
+++ src/utils/newPhTree.tsx
@@ -2,10 +2,10 @@ import { useNavigate } from "react-router-dom";
 import { createTree, myTreesCount } from "../api/phTree";
 import { getMe } from "../api/user";
 import { plans } from "../data/prices";
-import type { NotificationWS } from "../classes/NotificationWS";
 import { NotiFunc } from "../enums";
+import { notificationService } from "../classes/NotificationService";
 
-export const newPhTree = (notificationWS?: NotificationWS, history = useNavigate()) => async () => {
+export const newPhTree = (history = useNavigate()) => async () => {
     const { myTrees } = await myTreesCount({}) ?? {};
     const { plan } = await getMe({}) ?? {};
     if(!plan || myTrees === undefined) return;
@@ -14,7 +14,7 @@ export const newPhTree = (notificationWS?: NotificationWS, history = useNavigate
         name: "Untitled Phylogenetic Tree",
         isPublic: false
     }) ?? {};
-    notificationWS?.emit({
+    notificationService.emit({
         fun: NotiFunc.TREE,
         treeId: id
     })
