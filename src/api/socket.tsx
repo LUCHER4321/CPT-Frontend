e0a0d23eb48eacb8581b7e4e9502f351744958e4
LUCHER4321 <lucher4321@gmail.com>
2025-09-03 19:36:57 -0400

    Update socket.tsx

--- src/api/socket.tsx
+++ src/api/socket.tsx
@@ -1,4 +1,9 @@
 import { io } from "socket.io-client";
 import { WS_URL } from "../config";
 
-export const socket = io(WS_URL);
+export const socket = io(WS_URL, {
+    withCredentials: true
+});
+
+socket.on("connect", () => console.log("Connected to WS with ID:", socket.id));
+socket.on("disconnect", () => console.log("Disconnected from WS"));
