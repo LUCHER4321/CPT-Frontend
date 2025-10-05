68a0cc290c421d5c03eaefa9b0729ab1be17ae96
LUCHER4321 <lucher4321@gmail.com>
2025-10-05 18:32:40 -0300

    Create PayPalProvider.tsx

---
+++ src/components/pricing/PayPalProvider.tsx
@@ -0,0 +1,23 @@
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import { PAYPAL_ID } from "../../config";
+
+interface PayPalProviderProps {
+    children: any;
+}
+
+export const PayPalProvider = ({
+    children
+}: PayPalProviderProps) => {
+    return (
+        <PayPalScriptProvider
+            options={{
+                clientId: PAYPAL_ID,
+                components: "buttons",
+                currency: "USD",
+                vault: true
+            }}
+        >
+            {children}
+        </PayPalScriptProvider>
+    )
+}
