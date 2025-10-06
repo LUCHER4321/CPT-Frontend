86a65d25a5947e9bac1bbcedc1241fbd948a4401
LUCHER4321 <lucher4321@gmail.com>
2025-10-06 10:43:07 -0300

    Update PlansGrid.tsx

--- src/components/pricing/PlansGrid.tsx
+++ src/components/pricing/PlansGrid.tsx
@@ -1,24 +1,25 @@
 import { plans } from "../../data/prices";
-import type { Plan } from "../../enums";
+import type { UserResponse } from "../../types";
 import { PlanCard } from "./PlanCard";
 
 interface PlansGridProps {
     monthly?: boolean;
-    currentPlan?: Plan;
+    user?: UserResponse;
 }
 
 export const PlansGrid = ({
     monthly,
-    currentPlan
+    user
 }: PlansGridProps) => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full mb-12">
             {[...plans.keys()].map((p, index) => <PlanCard
+                user={user}
                 plan={p}
                 monthly={monthly}
                 key={index}
             >
-                {currentPlan ? currentPlan === p ? "Your current plan" : `${index > [...plans.keys()].indexOf(currentPlan) ? "Up" : "Down"}grade to ${plans.get(p)?.name}` : `Start with ${plans.get(p)?.name} Plan`}
+                {user ? user.plan === p ? "Your current plan" : `${index > [...plans.keys()].indexOf(user.plan) ? "Up" : "Down"}grade to ${plans.get(p)?.name}` : `Start with ${plans.get(p)?.name} Plan`}
             </PlanCard>)}
         </div>
     )
