fe8801ae53dd9363021897e98f1c3d42b9b2f16e
LUCHER4321 <lucher4321@gmail.com>
2025-10-11 23:45:55 -0300

    Update Auth.tsx

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -77,7 +77,7 @@ export const Auth = ({
                         billing={billing}
                         setBilling={setBilling}
                         goToAccount={(u) => {
-                            if(u) history("/account");
+                            if(u) history(plan === Plan.FREE ? "/account" : "/pricing");
                         }}
                     />
                 </div>
