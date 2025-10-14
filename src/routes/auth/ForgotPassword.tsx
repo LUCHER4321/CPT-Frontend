3debfa31e32e78fa80b011aa4622951c85025f9a
LUCHER4321 <lucher4321@gmail.com>
2025-10-13 22:46:40 -0300

    Create ForgotPassword.tsx

---
+++ src/routes/auth/ForgotPassword.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { HomeNavBar } from "../../components/home/HomeNavBar";
+import { AuthBanner } from "../../components/auth/AuthBanner";
+import { title } from "../../data/classNames";
+import { ForgotForm } from "../../components/auth/ForgotForm";
+
+export const ForgotPassword = () => {
+    const [email, setEmail] = useState("");
+    const [open, setOpen] = useState(false);
+    const [search, setSearch] = useState("");
+    return (
+        <>
+            <HomeNavBar
+                open={open}
+                setOpen={setOpen}
+                search={search}
+                setSearch={setSearch}
+            />
+            <div className="flex flex-col sm:flex-row w-screen sm:min-h-[100vh] p-0!">
+                <div className="sm:min-w-125 px-8 py-12 flex flex-col justify-center relative">
+                    <h1 className={"absolute top-12 right-8 left-8 text-center " + title}>Forgot your password?</h1>
+                    <ForgotForm
+                        email={email}
+                        setEmail={setEmail}
+                    />
+                </div>
+                <AuthBanner/>
+            </div>
+        </>
+    )
+}
