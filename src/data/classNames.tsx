9aaeb25f935c8fa9b618a2193cda84f827d1a352
LUCHER4321 <lucher4321@gmail.com>
2025-10-13 22:46:43 -0300

    Update classNames.tsx

--- src/data/classNames.tsx
+++ src/data/classNames.tsx
@@ -1,4 +1,5 @@
-export const filledButton = "dark:text-black text-white rounded-full! bg-[#1B5E20]! dark:bg-[#D8EDD9]! hover:bg-black! dark:hover:bg-white!";
+export const unborder= "border-0! focus:outline-0! focus-visible:outline-0!";
+export const filledButton = "dark:text-black text-white rounded-full! bg-[#1B5E20]! dark:bg-[#D8EDD9]! hover:bg-black! dark:hover:bg-white! " + unborder;
 export const borderButton = "text-black dark:text-white rounded-full! border-[#1B5E20]! dark:border-[#D8EDD9]! hover:bg-[#1B5E20]/100! dark:hover:bg-[#D8EDD9]/100! bg-black/0! hover:text-white dark:hover:text-black";
 export const blankButton = "text-black dark:text-white bg-black/0! rounded-full! border-black/0! px-2! focus:outline-0! focus-visible:outline-0!"
 export const title = "font-bold text-[#1B5E20]! dark:text-[#D8EDD9]!"
@@ -7,6 +8,5 @@ export const aText = "text-[#1B5E20]! dark:text-[#D8EDD9]! hover:underline! curs
 export const selectedBilling = "text-[#1B5E20] dark:text-[#D8EDD9] font-semibold text-lg transition-all duration-500"
 export const unselectedBilling = "font-medium text-lg transition-all duration-500"
 export const photoClass = "rounded-full aspect-square w-10! h-10! flex justify-center object-cover";
-export const unborder= "border-0! focus:outline-0! focus-visible:outline-0!";
 export const circleImage = "object-cover rounded-full";
 export const accountContainer = "w-screen! flex flex-col-reverse sm:flex-row justify-between h-screen sm:justify-start overflow-hidden relative";
