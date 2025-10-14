77b1b623d940708a28271450b4237cd61de63bc0
LUCHER4321 <lucher4321@gmail.com>
2025-10-13 23:39:22 -0300

    Update RecoverPassword.tsx

--- src/routes/auth/RecoverPassword.tsx
+++ src/routes/auth/RecoverPassword.tsx
@@ -3,15 +3,17 @@ import { HomeNavBar } from "../../components/home/HomeNavBar";
 import { title } from "../../data/classNames";
 import { AuthBanner } from "../../components/auth/AuthBanner";
 import { RecoverForm } from "../../components/auth/RecoverForm";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const RecoverPassword = () => {
     const [open, setOpen] = useState(false);
     const [search, setSearch] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [visiblePassword, setVisiblePassword] = useState(false);
     const [passwordC, setPasswordC] = useState("");
     const { token } = useParams();
+    const history = useNavigate();
     return (
         <>
             <HomeNavBar
@@ -24,6 +26,8 @@ export const RecoverPassword = () => {
                 <div className="sm:min-w-125 px-8 py-12 flex flex-col">
                     <h1 className={"text-center mb-10 " + title}>Create a new password</h1>
                     <RecoverForm
+                        email={email}
+                        setEmail={setEmail}
                         password={password}
                         setPassword={setPassword}
                         visiblePassword={visiblePassword}
@@ -31,6 +35,7 @@ export const RecoverPassword = () => {
                         confirmPassword={passwordC}
                         setConfirmPassword={setPasswordC}
                         token={token}
+                        goToAuth={() => history("/auth")}
                     />
                 </div>
                 <AuthBanner/>
