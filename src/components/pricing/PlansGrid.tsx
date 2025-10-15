319f1cfa5fa387453d40761d924c2b0ea18e7648
LUCHER4321 <lucher4321@gmail.com>
2025-10-14 22:06:56 -0300

    Update PlansGrid.tsx

--- src/components/pricing/PlansGrid.tsx
+++ src/components/pricing/PlansGrid.tsx
@@ -1,21 +1,24 @@
 import { plans } from "../../data/prices";
 import type { Plan } from "../../enums";
 import type { UserResponse } from "../../types";
+import { nullableInput } from "../../utils/nullableInput";
 import { PlanCard } from "./PlanCard";
 
 interface PlansGridProps {
     monthly?: boolean;
     user?: UserResponse;
-    plan?: Plan
+    plan?: Plan;
+    className?: string;
 }
 
 export const PlansGrid = ({
     monthly,
     user,
-    plan
+    plan,
+    className
 }: PlansGridProps) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full mb-12">
+        <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full mb-12 " + className}>
             {[...plans.keys()].map((p, index) => <PlanCard
                 user={user}
                 plan={p}
@@ -23,7 +26,7 @@ export const PlansGrid = ({
                 monthly={monthly}
                 key={index}
             >
-                {user ? user.plan === p ? "Your current plan" : `${index > [...plans.keys()].indexOf(user.plan) ? "Up" : "Down"}grade to ${plans.get(p)?.name}` : `Start with ${plans.get(p)?.name} Plan`}
+                {!Number.isNaN(nullableInput(p, p1 => plans.get(p1))?.month) ? user ? user.plan === p ? "Your current plan" : `${index > [...plans.keys()].indexOf(user.plan) ? "Up" : "Down"}grade to ${plans.get(p)?.name}` : `Start with ${plans.get(p)?.name} Plan` : "Contact Us"}
             </PlanCard>)}
         </div>
     )
