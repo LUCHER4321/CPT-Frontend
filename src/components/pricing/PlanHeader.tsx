113c96e5b464bb7c24834d4fe03f0d406fad6167
LUCHER4321 <lucher4321@gmail.com>
2025-10-14 22:07:04 -0300

    Update PlanHeader.tsx

--- src/components/pricing/PlanHeader.tsx
+++ src/components/pricing/PlanHeader.tsx
@@ -20,10 +20,11 @@ export const PlanHeader = ({
         <div className="mb-6 text-center">
             <h3 className={`text-2xl font-bold mb-2 ${price.color?.light} ${price.color?.dark}`}>{price.name}</h3>
             <div className="mb-2">
-                <span className="text-[2.5rem] font-bold">${dollars}</span>
-                <span className="text-xl font-bold">.{cents}</span>
-                <span>/month</span>
-                <span className={"w-full block text-[#1B5E20] dark:text-[#D8EDD9] transition-all duration-500 " + (monthly || price.year <= 0 ? "text-black/0!" : "")}>${price.year.toFixed(2)}/year</span>
+                {dollars !== "NaN" && <span className="text-[2.5rem] font-bold">${dollars}</span>}
+                {dollars === "NaN" && <span className="text-[2.5rem] font-bold">Contact Us</span>}
+                {dollars !== "NaN" && <span className="text-xl font-bold">.{cents}</span>}
+                {dollars !== "NaN" && <span>/month</span>}
+                {dollars !== "NaN" && <span className={"w-full block text-[#1B5E20] dark:text-[#D8EDD9] transition-all duration-500 " + (monthly || price.year <= 0 ? "text-black/0!" : "")}>${price.year.toFixed(2)}/year</span>}
             </div>
             <p className="">{children}</p>
         </div>
