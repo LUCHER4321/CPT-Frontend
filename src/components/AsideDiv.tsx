46eca7a5bc307dd818122452bff5bf07d35db232
LUCHER4321 <lucher4321@gmail.com>
2025-10-26 20:18:28 -0300

    Update AsideDiv.tsx

--- src/components/AsideDiv.tsx
+++ src/components/AsideDiv.tsx
@@ -2,8 +2,8 @@ import { nullableInput } from "../utils/nullableInput";
 
 export const AsideDiv = () => {
     const aside = document.querySelector("aside");
-    const marginTop = nullableInput(aside?.clientHeight, h => 2 * h);
+    const paddingTop = nullableInput(aside?.clientHeight, h => 2 * h);
     return (
-        <div style={{ marginTop }} className="block sm:hidden"/>
+        <div style={{ paddingTop }} className="block sm:hidden bg-[#D8EDD9] dark:bg-[#1B5E20] border-t border-t-[#D8EDD9] dark:border-t-[#1B5E20]"/>
     )
 }
