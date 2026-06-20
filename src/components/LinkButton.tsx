071502caabeb35b1c537c759eeeaf9bea80a23ed
LUCHER4321 <lucher4321@gmail.com>
2026-06-19 23:30:39 -0400

    Update LinkButton.tsx

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -1,15 +1,17 @@
 interface LinkButtonProps {
     href?: string;
-    className?: string
+    className?: string;
+    blank?: boolean;
     children?: any;
 }
 
 export const LinkButton = ({
     href,
     className,
+    blank,
     children
 }: LinkButtonProps) => {
     return (
-        <a href={href}><button className={className}>{children}</button></a>
+        <a href={href} target={blank ? "_blank" : undefined}><button className={className}>{children}</button></a>
     );
 };
