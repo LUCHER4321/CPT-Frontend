7d3bde0a9731e95eb360a07da1e8953852931451
LUCHER4321 <lucher4321@gmail.com>
2026-06-23 13:00:09 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { contact } from "../api/contact";
+import { DISCORD, GITHUB } from "../config";
 import { aText, filledButton, title } from "../data/classNames"
 import type { Email } from "../types";
 import { AuthField } from "./auth/AuthField";
@@ -50,8 +51,8 @@ export const Footer = ({
                         <LiLink href="/">Home</LiLink>
                         <LiLink href="/pricing">Pricing</LiLink>
                         <LiLink href="/trees">Search Trees</LiLink>
-                        <LiLink href="https://github.com/LUCHER4321/CPT-Frontend" blank>GitHub Repository</LiLink>
-                        <LiLink href="https://discord.gg/s9cJJHcnA4" blank>Discord Server</LiLink>
+                        <LiLink href={GITHUB} blank>GitHub Repository</LiLink>
+                        <LiLink href={DISCORD} blank>Discord Server</LiLink>
                         <LiLink href="https://lucher4321.github.io/Portafolio/" blank>Programmer's Portfolio</LiLink>
                     </ul>
                 </div>
